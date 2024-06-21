d5ec84d63ea07b3072acc6dc69880ae3b873e98a
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-06-21 22:10:31 +0300

    do task 3

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "./style.css";
 import "./basics/1";
 import "./basics/2";
-// import "./basics/3";
+import "./basics/3";
 // import "./basics/4";
 // import "./basics/5";
 // import "./basics/6";
